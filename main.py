44b332443786ef6a82d81c86e9cbd625869ee251
Ryan Boudwin <ryanboudwin@outlook.com>
2024-11-28 12:35:59 +0200

    getting valuecounts

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import pandas
 csvFile = pandas.read_csv('survey_results_public.csv')
 finland = csvFile[csvFile['Country'] == 'Finland']
 finland.to_csv("filtered_data.csv", index=False)
-print(finland)
+languages = finland['LanguageHaveWorkedWith'].dropna().str.split(';', expand=True).stack()
+word_counts = languages.value_counts()
+print("Languages I have Worked With:")
+print(word_counts)
+wantLanguages = finland['LanguageWantToWorkWith'].dropna().str.split(';', expand=True).stack()
+wantCount = wantLanguages.value_counts()
+print("Languages I want to work with:")
+print(wantCount)
